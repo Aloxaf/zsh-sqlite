e3daeb1106f5c96b8e836bd9a5f5243742b1ae77
Aloxaf <Aloxafx@gmail.com>
2022-11-27 17:45:49 +0800

    ci: add ci

--- zsh-sqlite.plugin.zsh
+++ zsh-sqlite.plugin.zsh
@@ -4,10 +4,11 @@
 0="${${(M)0:#/*}:-$PWD/$0}"
 
 ZSH_SQLITE_HOME=${0:A:h}
+ZSH_SQLITE_ZSH_SRC=5.8.1
 
 autoload -Uz is-at-least
 
--zsh-sqlite-build() {
+zsh-sqlite-build() {
   local ret macos nproc
   if [[ $OSTYPE == darwin* ]]; then
     macos=true
@@ -16,7 +17,7 @@ autoload -Uz is-at-least
     nproc=$(nproc)
   fi
 
-  pushd $ZSH_SQLITE_HOME/zsh/$1
+  pushd $ZSH_SQLITE_HOME/zsh/$ZSH_SQLITE_ZSH_SRC
   [[ -f ./configure ]] || ./Util/preconfig
   LIBS=-lsqlite3 ./configure --disable-gdbm --disable-pcre --without-tcsetpgrp ${macos:+DL_EXT=bundle}
   make -j$nproc
@@ -28,34 +29,33 @@ autoload -Uz is-at-least
     return $ret
   fi
 
-  pushd $ZSH_SQLITE_HOME/zsh/$1/Src/Modules
+  pushd $ZSH_SQLITE_HOME/zsh/$ZSH_SQLITE_ZSH_SRC/Src/Modules
   mkdir -p aloxaf
   mv sqlite.so aloxaf/
   rm -f *.so
   popd
 
-  print -P "%F{green}%BThe module has been built successfully.%f%b"
+  print -P "%F{green}%BThe module has been built successfully. Please restart zsh to apply it.%f%b"
 }
 
 () {
-  local require_version
-  #if is-at-least 5.8.1; then
-    require_version=5.8.1
-  #fi
+  if is-at-least 5.4.2; then
+    ZSH_SQLITE_ZSH_SRC=5.8.1
+  fi
 
-  local sqlite_module_path=$ZSH_SQLITE_HOME/zsh/$require_version/Src/Modules
+  local sqlite_module_path=$ZSH_SQLITE_HOME/zsh/$ZSH_SQLITE_ZSH_SRC/Src/Modules
   if (( ! $module_path[(I)$sqlite_module_path] )); then
     module_path+=($sqlite_module_path)
   fi
 
   if [[ -z $sqlite_module_path/aloxaf/sqlite.(so|bundle)(#qN) ]]; then
-    -zsh-sqlite-build $require_version
+    print -P "%F{yellow}%BThe module hasn't been build. Please build it with 'zsh-sqlite-build'%f%b"
+    return 1
   fi
 
   zmodload aloxaf/sqlite
   if [[ $SQLITE_MODULE_VERSION != "0.1.1" ]]; then
-    zmodload -u aloxaf/sqlite
-    -zsh-sqlite-build $require_version
-    zmodload aloxaf/sqlite
+    print -P "%F{yellow}%BThe module is outdate. Please rebuild it with 'zsh-sqlite-build'%f%b"
+    return 1
   fi
 }
